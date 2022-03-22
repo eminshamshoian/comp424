1c2ad8003fae245381bd913e10c1e72a9a8d2370
Emin <eminshamshoian@gmail.com>
2022-03-22 16:23:06 -0700

    first commit

---
+++ resources/js/Jetstream/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+            <div class="mt-8 text-2xl">
+                Helllo an welcome (username) your last login was at (time date)
+            </div>
+
+            <div class="mt-8 text-2xl">Download file</div>
+
+            <div class="mt-6 text-gray-500"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import JetApplicationLogo from "@/Jetstream/ApplicationLogo.vue";
+
+export default defineComponent({
+    components: {
+        JetApplicationLogo,
+    },
+});
+</script>
